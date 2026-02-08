537d4058a7f57b7a83cb4911604a996ffc823bfd
YZZlD <max.verhulst@hotmail.com>
2026-02-08 13:48:33 -0700

    feat: static location weather cards + search filtering for specific locations

--- Assignment1/src/App.jsx
+++ Assignment1/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+import WeatherDisplay from './components/WeatherDisplay'
+import './styles/WeatherDisplay.css'
 import './styles/App.css'
+import WeatherData from './data/locations.json'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [search, setSearch] = useState('');
 
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  }
   return (
     <>
-      <h1>Weather App</h1>
+
+      <div className='app'>
+        <h1 className='title'>Weather App</h1>
+        <input type="search" className='search-input' onChange={handleSearchChange}></input>
+        {WeatherData.map((location) => {
+          if(search == location.city.substring(0, search.length).toLowerCase()) return <WeatherDisplay city={location.city} temperature={location.temperature} weather={location.weather}></WeatherDisplay>;
+        })}
+      </div>
+
     </>
   )
 }
---
+++ Assignment1/src/components/WeatherDisplay.jsx
@@ -0,0 +1,14 @@
+import '../styles/WeatherDisplay.css';
+
+export default function WeatherDisplay(props) {
+
+    return (
+        <>
+            <div className='weather-display'>
+                <h1 className='city'>{props.city}</h1>
+                <p className='weather-info'>{props.temperature}</p>
+                <p className='weather-info'>{props.weather}</p>
+            </div>
+        </>
+    )
+}
